f2abd7b1acf69308c457cb051d9908c6c6abd6ad
Diego Vega <88682677+A01704492@users.noreply.github.com>
2022-11-25 07:26:35 -0600

    Add files via upload

--- Act 3.4 - Actividad Integral de BST (Evidencia Competencia)/main.cpp
+++ Act 3.4 - Actividad Integral de BST (Evidencia Competencia)/main.cpp
@@ -1,13 +1,55 @@
-/* 
+// =================================================================
+//
+// File: main.cpp
+// Author: Diego Vega Camacho & Manuel Villalpando Linares
+// Date:20/11/222
+//
+// =================================================================
+#include <iostream>
+#include <fstream>
+#include "minheap.h"
+int main(int argc, char *argv[])
+{
+	std::ifstream inputFile;
+	std::ofstream outputFile;
 
-==================================================================================================
-=
-=   Equipo:
-=   Manuel Villalpando Linares - A01352033
-=   Diego Vega Camacho - A01704492
-=
-=   Gitgub: 
-=
-=================================================================================================== 
+	if (argc != 3)
+	{
+		std::cout << "Uso: " << argv[0] << " Input.txt Output.txt";
+		return -1;
+	}
 
-*/
+	inputFile.open(argv[1]);
+	if (inputFile.fail())
+	{
+		std::cout << "No such input.";
+		return -1;
+	}
+
+	outputFile.open(argv[2]);
+	if (outputFile.fail())
+	{
+		std::cout << "Couldn't perform such output.";
+		return -1;
+	}
+
+	int N, elem;
+	inputFile >> N;
+
+	MinHeap<int> heap;
+	for (int i = 0; i < N; i++)
+	{
+		inputFile >> elem;
+		heap.push(elem);
+	}
+
+	int acum = 0;
+	int sum;
+	while (heap.size() > 1)
+	{
+		sum = heap.pop() + heap.pop();
+		heap.push(sum);
+		acum += sum - 1;
+	}
+	outputFile << acum << std::endl;
+}
